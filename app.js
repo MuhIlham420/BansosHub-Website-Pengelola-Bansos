ebcddfb605f39f1d1485aeef9217058e2ca83cf9
Iftitah Putri Syahna <24iftitah@mhs.usk.ac.id>
2025-05-23 18:21:07 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
 const authRoutes = require('./routes/authRoutes');           // untuk login / register
 const penerimaRoutes = require('./routes/penerimaRoutes');
 const penyediaRoutes = require('./routes/penyediaRoutes');
 
+app.use(cors());
+
 // middleware
 app.use(express.json());
 
 // route groups
-app.use('/', authRoutes);
+app.use('/api', authRoutes);
 app.use('/penerima', penerimaRoutes);
 app.use('/penyedia', penyediaRoutes);
 
